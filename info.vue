a8cebdf87ee5c6734043a900d463d91afe948194
Jerome Avecilla <102340560+javecilla@users.noreply.github.com>
2024-12-06 01:27:51 -0800

    Create info.vue

---
+++ info.vue
@@ -0,0 +1,9 @@
+<script setup>...</script>
+
+<template>
+  <Message>
+      For confidentiality of this project src code is not showing in this repository.
+  </Message>
+</template>
+
+<style scoped>...</style>
