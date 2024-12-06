090a42dbe69561ed2daa37f071b5bd9f1e4bcbc2
Jerome Avecilla <102340560+javecilla@users.noreply.github.com>
2024-12-06 01:53:30 -0800

    Update info.vue

--- info.vue
+++ info.vue
@@ -1,9 +1,9 @@
 <script setup>...</script>
 
 <template>
-  <Message>
+   <Message>
       For confidentiality of this project src code is not showing in this repository.
-  </Message>
+   </Message>
 </template>
 
 <style scoped>...</style>
